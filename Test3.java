f2dd37ff343269e43e80ca54d0864f9d7a233963
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-14 11:09:45 +0530

    Create Test3.java

--- Test3.java
+++ Test3.java
@@ -5,7 +5,7 @@ public class Test3
 public static void main(string[] args)
 {
  scanner s=new Scanner(System.in);
- char b=s.next().charAt(0);
+ char b=s.next().charAt(0).toLowerCase();
  if(a==b||e==b||i==b||o==b||u==b)
  {
   System.out.println("VOWLES");
