35843bf7c8b582944089558d030ad26d766b2961
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-14 09:39:55 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Test1
+{
+public static void main(String[] args)
+{
+   int n=Integer.parseInt(a[0]);
+   if(n>0)
+   {
+     System.out.println("number is positive"+n);
+   }
+   elseif(n<0)
+   {
+     System.out.println("number is negative"+n);
+     }
+     else
+     {
+      System.out.println("number is string or zero")
+      }
+      }
+      }
