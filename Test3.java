4e542ec589e909469583b670a310858065613c17
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-14 10:27:38 +0530

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Test3
+{
+public static void main(string[] args)
+{
+ scanner s=new Scanner(System.in);
+ char b=s.next().charAt(0);
+ if(a==b||e==b||i==b||o==b||u==b)
+ {
+  System.out.println("VOWLES");
+  }
+ else
+ {
+ System.out.println("CONSONANTS")
+ }
+ }
+ }
