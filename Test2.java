55cd47640394076d1de685d803174b3bc3c74605
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-14 09:54:26 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Test2
+{
+ public static void main(String[] args)
+ {
+   Scanner n=new Scanner(System.in);
+   int b=s.nextInt();
+   if(b%2==0)
+   {
+     System.out.println("the output is even");
+    }
+  else
+  {
+   System.out.println("the output is odd");
+   }
+   }
+   }
