570353586ec106f181ec40d25c4e8e3de4eeb932
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 10:33:32 +0530

    Create Test7.java

---
+++ Test7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Test7
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+for(int i=0;i<5;i++)
+{
+  System.out.println("HELLO");
+ }
+ }
+ }
+
