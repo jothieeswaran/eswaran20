3207430963cc7dbab653b14d7eca59c331984e24
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 11:20:45 +0530

    Create Test9.java

---
+++ Test9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Test9
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int sum=0;
+int n=s.nextInt();
+for(int i=0;i<n;i++)
+{
+ sum+=i;
+ }
+System.out.println(sum);
+}
+}
