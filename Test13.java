b674c4155697b55b69d65f1fd4ec04bd5b609eed
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 12:25:27 +0530

    Create Test13.java

---
+++ Test13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Test13
+{
+public Static void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int n=s.nextInt();
+if(n/n==1)
+{
+System.out.println("PRIME NUMBER");
+}
+else
+{
+System.out.println("NOT A PRIME")
+}
+}}
