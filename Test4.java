c43d803e241db3f0d565d40887dfa00595fbc298
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 09:40:33 +0530

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 {
 Scanner s=new Scanner(System.in);
 char c=s.next().charAt[0];
-if(((c=<A)&&(c=<Z))||((c=>a)&&(c=<z)))
+if(((c=>A)&&(c=<Z))||((c=>a)&&(c=<z)))
 {
   System.out.println("CHARACTER");
   }
