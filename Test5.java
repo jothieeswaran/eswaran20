1e085f9327657fea1dade62bc109f3cb7ccc0891
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 09:53:45 +0530

    Create Test5.java

---
+++ Test5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Test5
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int a=s.nextInt();
+int b=s.nextInt();
+int c=s.nextInt();
+if(a>b&&a>c)
+{
+  System.out.println("a is a larger"+a);
+  }
+ elseif
+ {
+   System.out.println("b is a larger"+b);
+   }
+else
+{
+  System.out.println("c is a larger"+c);
+  }
+  
+  }
+  }
