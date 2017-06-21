498e35b00ec0508c63b48b21962e8b1d3de65873
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 11:58:04 +0530

    Create Test11.java

---
+++ Test11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Test11
+{
+public static  void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int a=s.nextInt();
+int b=s.nextInt();
+int sum=0;
+   sum=(int)(Math.pow(a,b));
+   System.out.println(sum);
+   }}
