f82c8bde117f9724365f4699cbcef1db541948bd
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 12:15:08 +0530

    Create Test12.java

---
+++ Test12.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Test12
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int a=s.nextInt();
+int b=a%10;
+int c=(c*10)+b;
+int d=a/10;
+if(a==c){
+   System.out.println("PALINDROME");
+   }
+else
+{
+ System.out.println("NOT PALINDROME");
+ }
+ }}
+ 
