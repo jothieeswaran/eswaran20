508b7c140a7e50182c1acda750109153e6832f56
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-21 10:25:04 +0530

    Create Test6.java

---
+++ Test6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Test6
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in)
+int a=s.nextInt();
+if(a%4==0)
+{
+ System.out.println("LEAP YEAR");
+ }
+ else
+ {
+  System.out.println("NOT A LEAP YEAR");
+  }
+  }
+  }
