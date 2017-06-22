8c83959ed3c2723ea8582c1e519bcee370c414af
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-22 14:54:29 +0530

    Update Test13.java

--- Test13.java
+++ Test13.java
@@ -5,14 +5,20 @@ public Static void main(String[] args)
 {
 Scanner s=new Scanner(System.in)
 int n=s.nextInt();
+  int Flag=0;
   for(int i=2;i<n;i++)
   {
 if(n%i==0)
 {
 System.out.println(" NOT A PRIME NUMBER");
+  Flag=1;
+  break;
 }
-else
+  }
+if(Flag=0)
 {
 System.out.println("PRIME");
 }
-}}}
+}
+
+}}
