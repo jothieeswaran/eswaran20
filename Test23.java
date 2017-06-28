752bf3f15c85f395e4dd2e55ec60c04c02281f64
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-28 09:03:44 -0700

    Create Test23.java

---
+++ Test23.java
@@ -0,0 +1,11 @@
+public class Test23
+{
+public static void main(String[] args)
+{
+ char[] b=args[0].tocharArray();
+ for(int i=0;i<args[0].length;i++)
+ {
+ System.out.println((int)c[i]);
+ }
+ }
+ }
