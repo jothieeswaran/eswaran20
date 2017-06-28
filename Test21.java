3869af5b25cd9ccf8f3f46b7a20d0cccd496c1c7
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-28 10:02:28 +0530

    Create Test21.java

---
+++ Test21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class  Test21{
+        public static void main(String args[]) {
+ 
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=0,b=1,c,n;
+       
+        for(int i=2;i<n;++i)
+        {
+           c=a+b;
+           System.out.println(c);
+           a=b;
+           b=c;
+           }
+           }
+           }
+        
+
