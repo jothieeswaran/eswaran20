1b1e2d56df151bd9d62ab1e6686c0d70a9e95b53
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-28 08:32:39 -0700

    Create Test22.java

---
+++ Test22.java
@@ -0,0 +1,10 @@
+import java.util;
+public class Test22
+{
+  public static void main(String[] args)
+  {
+  StringBuffer s=new StringBuffer(args[0]);
+  System.out.println(s.reverse());
+  }
+  }
+  
