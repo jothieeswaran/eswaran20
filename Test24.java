9f7bda4b12c5dc814161f14c12244df76a796c6e
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-28 09:18:27 -0700

    Create Test24.java

---
+++ Test24.java
@@ -0,0 +1,14 @@
+class Test24
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.next();
+int count=0;
+for(int i=0;i<str.length();i++)
+{
+  count++;
+  System.out.println(count);
+  }
+  }
+  }
