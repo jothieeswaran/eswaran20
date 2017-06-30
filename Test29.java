b9a270500b0983f2de3520c01fb234ab62cf98c9
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-30 12:02:46 +0530

    Create Test29.java

---
+++ Test29.java
@@ -0,0 +1,13 @@
+public class Test29
+{
+ public static void main(String [] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  int charcount=0;
+  for(int i=0;i<str.length();i++)
+  {
+      charcount++;
+      }
+      System.out.println(charcount);
+      }}
