66cddf11ad2db9c3fefad11aa0627d61f883ee26
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-30 09:49:46 +0530

    Create Test25.java

---
+++ Test25.java
@@ -0,0 +1,16 @@
+class Test25
+{
+public static void main(String[] args)
+{
+  Scanner s=new Scanner(System.in);
+  char c=s.next().charAt(0);
+  if(c=='q')
+  {
+    System.exit(0);
+    }
+   else
+   {
+     System.out.println("hi");
+     }
+     }
+     }
