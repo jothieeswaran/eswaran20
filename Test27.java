bb3c4043cda639f0da086180dfabca40c77dc3c6
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-30 10:28:55 +0530

    Create Test27.java

---
+++ Test27.java
@@ -0,0 +1,21 @@
+public class Test27{
+
+	public static void main(String[] args) {
+  int a,b;
+  intc=0;
+  for(a=1;a<=15;a++)
+  {
+    c=c+a;
+    System.out.println(c);
+    }
+    for(a=15;a<=45;a++)
+    {
+       if(a%2==1)
+       {
+       System.out.println("ODD NUMBER");
+       }
+       
+  }
+  }
+  }
+       
