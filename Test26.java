227d9352fa088105fa5b5c14359a6140eed47445
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-06-30 10:09:44 +0530

    Create Test26.java

---
+++ Test26.java
@@ -0,0 +1,14 @@
+class Test26
+{
+public static void main(String[] args)
+{
+   Scanner s= new Scanner(System.in);
+   int sum=0;
+   int n=s.next();
+   for(int i=0;i<=n;i++)
+   {
+     sum=sum+i;
+     }
+   System.out.println(sum);
+   }
+   }
