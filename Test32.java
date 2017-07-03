feac45a950a3339ba6cae221ddc80790aa21262b
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-03 15:32:15 +0530

    Update and rename Test31.java to Test32.java

---
+++ Test32.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Test32
+{
+public static void main(String ar[])
+{
+int a=0,b=0,temp=0;
+
+Scanner sc=new Scanner(System.in);
+a=sc.next();
+b=sc.next();
+     System.out.println(a+"    "+b);
+temp=a;
+     a=b;
+     b=temp;
+System.out.println(a+"      "+b);
+}}
