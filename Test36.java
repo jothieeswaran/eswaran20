9a7e8711d78c415b3c49887d7fae809bd5e49108
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 14:50:00 +0530

    Update and rename Test35.java to Test36.java

---
+++ Test36.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Test36
+{
+public static void main(String ar[])
+{
+int a=0,b=1,c=0;
+Scanner s=new Scanner(System.in);
+int n=s.next();
+ for(i=0;i<=n;i++)
+ {
+  a=b;
+  b=c;
+  c=a+b;
+  
+ }
+      System.out.println(c);   
+   
+    }}
