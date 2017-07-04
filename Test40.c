38a7efd4fdbdc7d18bbe01435d0eb09ba92e6593
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 15:24:42 +0530

    Update and rename Test39.java to Test40.c

---
+++ Test40.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+   int n;
+   if(n=>1&&n<=9)
+   {
+      printf("\n%d",n);
+   }
+   else
+   {
+      printf("\n enter the number not in range");
+   }
+   getch();
+}
