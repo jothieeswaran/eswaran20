1a63e33296e6582e12f534116b0b3a2d3ff98f25
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 15:15:06 +0530

    Update and rename Test38.java to Test39.java

--- Test39.java
+++ Test39.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class Test38
+public class Test39
 {
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
    String s1=s.next();
    String s2=s.next();
-   System.out.print(s1.compareTo(s2));
+   System.out.print(s1+s2);
    
     }}
