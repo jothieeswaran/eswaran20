644881dc5be402f060152f5530bbb146dad3105d
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 15:40:22 +0530

    Create Test42.java

---
+++ Test42.java
@@ -0,0 +1,12 @@
+public class calc{
+  public static void main(String[] args){
+int i,b,a;
+Scanner s=new Scanner(System.in);
+int a=s.next();
+for(i=1;i<=10;i++)
+{
+b=a*i;
+System.out.print("a"+"*"+"i"+"="+"b");
+
+
+}
