e790c97ff62dd2339fecf968021989f962782d8b
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 10:02:15 +0530

    Update and rename Test33.java to Test34.java

---
+++ Test34.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Test34
+{
+public static void main(String ar[])
+{
+int a=0,b=0,c=0;
+Scanner s=new Scanner(System.in);
+a=s.next();
+b=s.next();
+c=s.next();     
+if(a>b&&a>c)
+      {
+     System.out.println(a);
+       }
+elseif(b>a&&b>c)
+      {
+     System.out.println(b);
+       }
+   else
+   {
+      System.out.println(b);   
+   }
+    }}
