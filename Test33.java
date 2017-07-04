f2ac6ccacc7a091bbb7760ad76e6d91352e57717
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-04 09:51:36 +0530

    Update and rename Test32.java to Test33.java

--- Test33.java
+++ Test33.java
@@ -1,16 +1,17 @@
 import java.util.*;
-public class Test32
+public class Test33
 {
 public static void main(String ar[])
 {
-int a=0,b=0,temp=0;
+int a=0,b=0;
 
 Scanner sc=new Scanner(System.in);
 a=sc.next();
 b=sc.next();
      System.out.println(a+"    "+b);
-temp=a;
-     a=b;
-     b=temp;
+
+     a=a^b;
+     b=a^b;
+     a=a^b;
 System.out.println(a+"      "+b);
 }}
