57dcc7297b0ad761ca719f21f105a6aa268caafa
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-10 14:46:16 +0530

    Create Test43.java

---
+++ Test43.java
@@ -0,0 +1,12 @@
+class Test43
+{
+public static vod main(String[] args)
+{
+   Scanner s=new Scanner(System.in);
+   String st=s.next();
+   string str[]=st.Split(" ");
+   for((int i=str.length()-1);i>=0;i--)
+   {
+   System.out.println(i);
+   }
+   }}
