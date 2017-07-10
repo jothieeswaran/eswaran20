451ac3d98d23249005fe4e97e9bf74c8dec99fe8
jothieeswaran <jothieeswaranmurugan@gmail.com>
2017-07-10 14:54:21 +0530

    Update Test43.java

--- Test43.java
+++ Test43.java
@@ -4,9 +4,9 @@ public static vod main(String[] args)
 {
    Scanner s=new Scanner(System.in);
    String st=s.next();
-   string str[]=st.Split(" ");
+   string str[]=st.split("");
    for((int i=str.length()-1);i>=0;i--)
    {
-   System.out.println(i);
+   System.out.println(str.charAt(i));
    }
    }}
